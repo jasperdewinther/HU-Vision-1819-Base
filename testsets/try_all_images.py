51140d2e37130fc3867b37fde23b9d549f2242f3
jasperdewinther <jasper.dewinther@student.hu.nl>
2020-03-06 14:59:29 +0100

    added error counting

--- testsets/try_all_images.py
+++ testsets/try_all_images.py
@@ -5,6 +5,13 @@ import time
 import collections
 import numpy as np
 
+def count_matching(condition, seq):
+    """Returns the amount of items in seq that return true from condition"""
+    return sum(1 for item in seq if condition(item))
+
+def isError(i):
+	return i > 2000
+
 exe_path = "..\\source\\ExternalDLL\\Release\\ExternalDLL.exe"
 
 os.sep = '\\'
@@ -44,16 +51,22 @@ try:
     goodsPerBatch = np.full(len(results), 0)
     good = 0
     totalCount = 0
+    allResults = list()
 
     for key in results:
+		for i in results[key]:
+			allResults.append(i)
         goodsPerBatch[int(key)] = results[key].count(1)
         totalCount += len(results[key])
         good += results[key].count(1)
+	
+	totalErrors = count_matching(isError, allResults)
 except:
     pass
 try:
     print("standard deviation:", np.std(goodsPerBatch))
-    print("percentage correct faces recognised:", good/totalCount)
+    print("percentage correct faces recognised:", good/totalCount*100)
+	print("total errors:", str(totalErrors), "error percentage", str(totalErrors/totalCount*100))
 except:
     pass
 print("total execution time:", str(timeDiff), "seconds")
