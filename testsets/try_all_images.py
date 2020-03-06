47acaadf97dc97cd17b4cf062bbfeaf884384fbc
jasperdewinther <jasper.dewinther@student.hu.nl>
2020-03-06 14:22:05 +0100

    edited test scipt

--- testsets/try_all_images.py
+++ testsets/try_all_images.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import time
 import collections
+import numpy as np
 
 exe_path = "..\source\ExternalDLL\Release\ExternalDLL.exe"
 
@@ -10,7 +11,7 @@ os.sep = '\\'
 dir_path = os.path.dirname(os.path.realpath(__file__))
 images_path = os.path.join(dir_path, sys.argv[1])
 
-results = list()
+results = dict()
 
 timeDiff = 0
 
@@ -18,6 +19,8 @@ timeDiff = 0
 
 for subPathName in os.listdir(images_path):
     subPath = os.path.join(images_path, subPathName)
+    batchNumber = subPath.split("/")[len(subPath.split("/"))-1]
+    results[batchNumber]
     startTime = None
     endTime = None
     if os.path.isdir(subPath):
@@ -27,21 +30,30 @@ for subPathName in os.listdir(images_path):
             startTime = time.time()
             result = subprocess.call([exe_path, subSubPath])
             endTime = time.time()
-            results.append(result)
+            results[batchNumber].append(result)
     elif os.path.isfile(subPath):
         print("working on:", subPath)
         startTime = time.time()
         result = subprocess.call([exe_path, subPath])
         endTime = time.time()
-        results.append(result)
+        results[batchNumber].append(result)
     timeDiff += endTime-startTime
 
+try:
+    goodsPerBatch = np.full(len(results), 0)
+    good = 0
+    totalCount = 0
 
-
-
-
-print(collections.Counter(results).keys())
-print(collections.Counter(results).values())
-print("percentage correct faces recognised:", results.count(1)/len(results)*100)
+    for key in results:
+        goodsPerBatch[int(key)] = results[key].count(1)
+        totalCount += len(results[key])
+        good += results[key].count(1)
+except:
+    pass
+try:
+    print("standard deviation:", np.std(goodsPerBatch))
+    print("percentage correct faces recognised:", good/totalCount)
+except:
+    pass
 print("total execution time:", str(timeDiff), "seconds")
 print("avarage execution time:", str(timeDiff/len(results)), "seconds")
