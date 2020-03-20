3b68e0d58b4a4fbb267de788e58eb7051407fba7
Tonyvdk <t.r.vanderkrogt@gmail.com>
2020-03-20 17:05:33 +0100

    Added a simple threshold function

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -13,12 +13,24 @@ IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &i
 	//-2 on both width and heigth because of kernel
 	const int size = 9;
 	IntensityImage * newImage = new IntensityImagePrivate(image.getWidth() - size + 1, image.getHeight() - size + 1);
-	const auto basicKernel = basicKernels::Laplace;
+	const auto basicKernel = basicKernels::SobelY;
 	const auto kernel = Kernel<size>(basicKernel);
 	kernel.apply(image, newImage, false);
 	return newImage;
 }
 
 IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &image) const {
-	return nullptr;
+	const uint8_t Threshold = 30;
+	IntensityImage* newImage = new IntensityImagePrivate(image.getWidth(), image.getHeight());
+	for (int i = 0; i < image.getWidth(); i++) {
+		for (int j = 0; j < image.getHeight(); j++) {
+			if (image.getPixel(i, j) < Threshold) {
+				newImage->setPixel(i, j, 255);
+			}
+			else {
+				newImage->setPixel(i, j, 0);
+			}
+		}
+	}
+	return newImage;
 }
